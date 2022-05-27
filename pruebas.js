0de8eacbe90abaf9da3ef4dc611b10f928e44163
Gianne Paola <giannepaola27@gmail.com>
2022-05-27 18:14:27 -0300

    agregue slice, forEach

--- pruebas.js
+++ pruebas.js
@@ -1,92 +1,16 @@
-
-// resultadoNoche = 0
-// let resultado = 0;
-// let habitacionBasica = 40;
-// let habitacionDoble = 50;
-// let habitacionFamiliar = 60;
-// let habitacionSuit = 90 ;
-// document.addEventListener('DOMContentLoaded', function() {
-
-// const validarPersonas = (personas) => {
-
-// let resultadoPersonas = prompt("ingrese el numero de personas");
-// resultadoPersonas = personas;
-// if (resultadoPersonas> 0 && resultadoPersonas<= 8){
-
-// return true;
-
-// }else if (resultadoPersonas <= 0 || resultadoPersonas== " ") {
-
-// return alert("ingrese un numero valido. max 8 personas por habitacion");
-
-// }
-// else {
-
-// alert("lo siento, numero de personas maximo por habitacion 8");
-
-// }
-// }
-
-// const numeroNoches = (noches) => {
-// let resultadoNoche = prompt("ingrese el numero de noches");
-// resultadoNoche  = noches;
-// if (resultadoNoche > 0){
-
-//     return true;
- 
-// } else {
-// alert("ingrese numero valido")
-    
-// }
-// }
-
-// while(resultadoPersonas == true && resultadoNoche == true){
-// function tipoDeHabitacion(habitaciones){
-  
-//  let seleccionHabitacion = prompt('1-habitacion basica, 2-habitacion doble, 3-habitacion familiar, 4-habitacion suit');
-// seleccionHabitacion =habitaciones;
-// if(seleccionHabitacion == 1){
-
-//  resultado = personas * habitacionBasica * noches;
-
-// return  alert (`costo de habitacion basica: ${habitacionBasica}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) `);
-
-// } else if (seleccionHabitacion == 2) {
-
-//      resultado = personas * habitacionDoble * noches;
-//     return alert(`Costo de habitacion Doble ${habitacionDoble}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) `)
-
-// } else if (seleccionHabitacion == 3) {
-    
-//     resultado = personas * habitacionFamiliar * noches;
-//    return alert (`Costo habitacion Familiar ${habitacionFamiliar}, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) `);
-
-// }else if (seleccionHabitacion== 4) {
-    
-//    resultado = personas * habitacionSuit * noches;
-// return  alert (`Costo habitacion Suit ${habitacionSuit}, para ${resultadoPersonas} personas,por ${resultadoNoche}: ${resultado}$ (precio total) }`);
-
-// } else  {
-//     return  alert("error, ingrese un numero valido");
-// }
-
-// };
-
-// numeroPersonas();
-// numeroNoches();
-// tipoDeHabitacion();
-
-// }
-// }, false);
 let resultado = 0;
 //costo de habitaciones
-const habitaciones = {
-    habitacionBasica: 40,
-    habitacionDoble: 50,
-    habitacionFamiliar: 60,
-    habitacionSuit: 90
-}
+const habitaciones = [
+    { "habitacionBasica": 40,
+      "habitacionDoble": 50,
+      "habitacionFamiliar": 60,
+      "habitacionSuit": 90
+    }
+]
+let equipments = ['televisor','baño', 'wifi', 'paseos a caballo', 'skii', 'spa', 'desayunos'];
+
 document.addEventListener('DOMContentLoaded', function() {
+alert("BIENVENIDO. ESTE ES UN SISTEMA DE RESERVACION DE CABAÑAS EN EL HOTEL LASKA. TE BRINDAREMOS UN PRESUPUESTO DEPENDIENDO DEL NUMERO DE PERSONAS, DE NOCHES Y QUE TIPO DE HABITACION DESEAS. (LOS PRECIOS SON EN DOLARES) (MAXIMO POR CABAÑA 8 PERSONAS) GRACIAS POR ELEGIRNOS!!!♣♦");
 function reserva() {
 
 let resultadoPersonas = prompt("ingrese el numero de personas"); //preguntar numero de personas 
@@ -110,45 +34,48 @@ if (resultadoNoche == resultadoNoche){
     return console.error();
 }
 //costo de habitaciones
-const habitaciones = {
-    habitacionBasica: 40,
-    habitacionDoble: 50,
-    habitacionFamiliar: 60,
-    habitacionSuit: 90
-}
-for (let tipoDeHabitacion in habitaciones) {
+const habitaciones = [
+    { habitacionBasica:  40,
+      habitacionDoble: 50,
+      habitacionFamiliar: 60,
+      habitacionSuit: 90
+    }
+]
+habitaciones.forEach(habitacion => {
+let equipments = ['televisor','baño', 'wifi', 'paseos a caballo', 'skii', 'spa', 'desayunos'];
 //preguntar que tipo de habitacion
 let tipoDeHabitacion = prompt('1-habitacion basica (40$),  2-habitacion doble (50$),  3-habitacion familiar (60$), 4-habitacion suit (90$)');
-
 if( tipoDeHabitacion == 1){
-
- resultado = resultadoPersonas * habitaciones.habitacionBasica * resultadoNoche;
-
-return  alert (`costo de habitacion basica: ${habitaciones.habitacionBasica}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) `);
+let newEquipmentsBasica = equipments.slice(0,2)
+resultado = resultadoPersonas * habitacion["habitacionBasica"] * resultadoNoche;  //CALCULO DE PRECIO
+//RESPUESTA total
+return  alert (`costo de habitacion basica: ${habitacion["habitacionBasica"]}$, 
+para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total).
+\n INCLUYE: ${newEquipmentsBasica}`);
 
 } else if (tipoDeHabitacion == 2) {
-
-     resultado = resultadoPersonas *habitaciones.habitacionDoble * resultadoNoche;
-    return alert(`Costo de habitacion Doble ${habitaciones.habitacionDoble}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) `)
+let newEquipmentsDoble = equipments.slice(0,3)
+resultado = resultadoPersonas * habitacion["habitacionDoble"] * resultadoNoche;
+return alert(`Costo de habitacion Doble ${habitacion["habitacionDoble"]}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total). \n INCLUYE: ${newEquipmentsDoble} `)
 
 } else if (tipoDeHabitacion == 3) {
-    
-    resultado = resultadoPersonas * habitaciones.habitacionFamiliar * resultadoNoche;
-   return alert (`Costo habitacion Familiar ${habitaciones.habitacionFamiliar}, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) `);
+    let newEquipmentsFamiliar = equipments.slice(0,4)
+    resultado = resultadoPersonas * habitacion["habitacionFamiliar"] * resultadoNoche;
+   return alert (`Costo habitacion Familiar ${ habitacion["habitacionFamiliar"]}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total). \n INCLUYE: ${newEquipmentsFamiliar} `);
 
-}else if (tipoDeHabitacion == 4) {
-    
-   resultado = resultadoPersonas * habitaciones.habitacionSuit  * resultadoNoche;
-return  alert (`Costo habitacion Suit ${habitaciones.habitacionSuit}, para ${resultadoPersonas} personas,por ${resultadoNoche}: ${resultado}$ (precio total) }`);
+}else if (tipoDeHabitacion == 4) {  
+resultado = resultadoPersonas * habitacion["habitacionSuit"] * resultadoNoche;
+return  alert (`Costo habitacion Suit ${habitacion["habitacionSuit"]}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total)} \n INCLUYE: ${equipments}`);
 
 } else  {
     return  alert("error, ingrese un numero valido");
 }
-}
+
+});
 }
 reserva();
-
 }, false);
+
 
 
 
--- two.js
+++ two.js
@@ -1,15 +1,18 @@
 let resultado = 0;
 //costo de habitaciones
 const habitaciones = [
-    { habitacionBasica: 40,},
-    {habitacionDoble: 50,},
-    { habitacionFamiliar: 60,},
-    {habitacionSuit: 90},
+    { habitacionBasica:  40,
+      habitacionDoble: 50,
+      habitacionFamiliar: 60,
+      habitacionSuit: 90
+    }
 ]
-const equipment = ['televisor','baño', 'wifi', 'paseos a caballo', 'skii', 'spa', 'desayunos'];
+
+let equipments = ['televisor','baño', 'wifi', 'paseos a caballo', 'skii', 'spa', 'desayunos'];
+
 
 document.addEventListener('DOMContentLoaded', function() {
-    alert("BIENVENIDO. ESTE ES UN SISTEMA DE RESERVACION DE CABAÑAS EN EL HOTEL LASKA. TE BRINDAREMOS UN PRESUPUESTO DEPENDIENDO DEL NUMERO DE PERSONAS, DE NOCHES Y QUE TIPO DE HABITACION DESEAS. (LOS PRECIOS SON EN DOLARES) (MAXIMO POR CABAÑA 8 PERSONAS) GRACIAS POR ELEGIRNOS!!!♣♦");
+alert("BIENVENIDO. ESTE ES UN SISTEMA DE RESERVACION DE CABAÑAS EN EL HOTEL LASKA. TE BRINDAREMOS UN PRESUPUESTO DEPENDIENDO DEL NUMERO DE PERSONAS, DE NOCHES Y QUE TIPO DE HABITACION DESEAS. (LOS PRECIOS SON EN DOLARES) (MAXIMO POR CABAÑA 8 PERSONAS) GRACIAS POR ELEGIRNOS!!!♣♦");
 function reserva() {
 
 let resultadoPersonas = prompt("ingrese el numero de personas"); //preguntar numero de personas 
@@ -34,47 +37,42 @@ if (resultadoNoche == resultadoNoche){
 }
 //costo de habitaciones
 const habitaciones = [
-    { "habitacionBasica": 40,
-      "habitacionDoble": 50,
-      "habitacionFamiliar": 60,
-      "habitacionSuit": 90
+    {habitacionBasica:  40,
+    habitacionDoble: 50,
+    habitacionFamiliar: 60,
+    habitacionSuit: 90
     }
 ]
-
-for (let habitacion of habitaciones) {
+habitaciones.forEach(habitacion => {
+let equipments = ['televisor','baño', 'wifi', 'paseos a caballo', 'skii', 'spa', 'desayunos'];
 //preguntar que tipo de habitacion
 let tipoDeHabitacion = prompt('1-habitacion basica (40$),  2-habitacion doble (50$),  3-habitacion familiar (60$), 4-habitacion suit (90$)');
-
 if( tipoDeHabitacion == 1){
-const equipment = ['televisor','baño', 'wifi', 'paseos a caballo', 'skii', 'spa', 'desayunos'];
-equipment.forEach(function(equipment) {
-let resultadoBasica = equipment.slice(0,1,2,)  //EQUIPAMIENTO INCLUIDO
+let newEquipmentsBasica = equipments.slice(0,2)
 resultado = resultadoPersonas * habitacion["habitacionBasica"] * resultadoNoche;  //CALCULO DE PRECIO
 //RESPUESTA total
 return  alert (`costo de habitacion basica: ${habitacion["habitacionBasica"]}$, 
-para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total).
- \n INCLUYE: ${resultadoBasica}`);
-});
- 
-} else if (tipoDeHabitacion == 2) {
+para ${resultadoPersonas} personas \n por ${resultadoNoche} noches. \n total = ${resultado}$
+\n INCLUYE: ${newEquipmentsBasica}`);
 
-     resultado = resultadoPersonas * habitacion["habitacionDoble"] * resultadoNoche;
-    return alert(`Costo de habitacion Doble ${habitacion["habitacionDoble"]}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) `)
+} else if (tipoDeHabitacion == 2) {
+let newEquipmentsDoble = equipments.slice(0,3)
+resultado = resultadoPersonas * habitacion["habitacionDoble"] * resultadoNoche;
+return alert(`Costo de habitacion Doble ${habitacion["habitacionDoble"]}$ \n para ${resultadoPersonas} personas \n por ${resultadoNoche} noches.  \n total =  ${resultado}$  \n INCLUYE: ${newEquipmentsDoble} `)
 
 } else if (tipoDeHabitacion == 3) {
-    
+    let newEquipmentsFamiliar = equipments.slice(0,4)
     resultado = resultadoPersonas * habitacion["habitacionFamiliar"] * resultadoNoche;
-   return alert (`Costo habitacion Familiar ${ habitacion["habitacionFamiliar"]}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) `);
+   return alert (`Costo habitacion Familiar ${ habitacion["habitacionFamiliar"]}$ \n para ${resultadoPersonas} personas \n por ${resultadoNoche} noches.  \n total  ${resultado}$  \n INCLUYE: ${newEquipmentsFamiliar} `);
 
-}else if (tipoDeHabitacion == 4) {
-    
-   resultado = resultadoPersonas * habitacion["habitacionSuit"] * resultadoNoche;
-return  alert (`Costo habitacion Suit ${habitacion["habitacionSuit"]}$, para ${resultadoPersonas} personas, por ${resultadoNoche} noches: ${resultado}$ (precio total) }`);
+}else if (tipoDeHabitacion == 4) {  
+resultado = resultadoPersonas * habitacion["habitacionSuit"] * resultadoNoche;
+return  alert (`Costo habitacion Suit ${habitacion["habitacionSuit"]}$ \n para ${resultadoPersonas} personas \n por ${resultadoNoche} noches.  \n total  ${resultado}$ \n INCLUYE: ${equipments}`);
 
 } else  {
     return  alert("error, ingrese un numero valido");
 }
-}
+});
 }
 reserva();
 
