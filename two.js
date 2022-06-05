6708702698ecfc3295eb68183918ea355978dce2
Gianne Paola <giannepaola27@gmail.com>
2022-06-05 15:48:23 -0300

    agregue evento click al sistema de reserva

--- two.js
+++ two.js
@@ -1,6 +1,7 @@
-let resultado = 0;
-let resultadoPersonas = [];
-let resultadoNoche = [];
+// let resultado = 0;
+// let resultadoPersonas = [];
+// let resultadoNoche = [];
+let operacion = [];
 //costo de habitaciones
 const habitaciones = [
     {habitacionBasica: 40,
@@ -24,69 +25,49 @@ roomEquipment.append(li)
 li.className = 'listaEquipments'
 })
 
-//DOM/EVENTOS: boton click para obtener info
+//DOM/EVENTOS: boton click para obtener info del proyecto
     function cambiar (){
         document.getElementById('laskatitulo').innerHTML = "BIENVENIDO. ESTE ES UN SISTEMA DE RESERVACION DE CABAÑAS EN EL HOTEL LASKA. TE BRINDAREMOS UN PRESUPUESTO DEPENDIENDO DEL NUMERO DE PERSONAS, DE NOCHES Y QUE TIPO DE HABITACION DESEAS. GRACIAS POR ELEGIRNOS!";
     }
     document.getElementById('boton1').onclick = function(){
         cambiar();
     }
-// //preguntar numero de personas 
-function validarPersonas(){
-    let preguntarPersonas = prompt("ingrese numero de personas (debe ser menor a 8)")
-    if (preguntarPersonas >= 1 && preguntarPersonas <= 8) {
-        resultadoPersonas.push(preguntarPersonas)
-        return validarNoches();
-    }  else {
-        alert("Número maximo 8 personas, ingrese un numero válido")
-        console.error();
+
+/*EJECUTAR PROGRAMA*/  
+let boton2 = document.getElementById('boton2');
+boton2.addEventListener("click", ()=> {calcular()} )
+
+function calcular (){
+    let capturarPersonas = parseInt(document.getElementById('invitados').value);
+    let capturarNoches = parseInt(document.getElementById('check-in').value);
+    let capturarHabitacion = document.getElementById('selectHabitacion').value;
+    let operacion = [];
+    habitaciones.forEach(habitacion => {
+    if(capturarHabitacion == "basica"){
+    operacion =  capturarPersonas * capturarNoches * habitacion["habitacionBasica"];
+    let newEquipmentsBasica = equipments.slice(0,2)
+    alert(`costo de habitacion basica: ${habitacion["habitacionBasica"]}$ \n invitados: ${capturarPersonas} \n noches ${capturarNoches} \n total ${operacion}$
+    \n incluye: ${newEquipmentsBasica} `)
+    } 
+    else if (capturarHabitacion == "doble"){
+        operacion =  capturarPersonas * capturarNoches * habitacion["habitacionDoble"]; 
+        let newEquipmentsDoble = equipments.slice(0,3)
+        alert(` Costo de habitacion Doble ${habitacion["habitacionDoble"]}$ \n invitados: ${capturarPersonas} \n noches ${capturarNoches} \n total ${operacion}$
+        \n incluye: ${newEquipmentsDoble} `)
     }
-}  
-validarPersonas();
-// preguntar numero de noches
-function validarNoches(){
-    let preguntarNoches = prompt("ingrese el numero de noches"); 
-    if (preguntarNoches <= 365){
-      resultadoNoche.push(preguntarNoches);
-        return reserva();
-    } else {
-        alert("Ingrese un número válido")
-        console.error();
+    else if (capturarHabitacion == "familiar"){
+        operacion =  capturarPersonas * capturarNoches * habitacion["habitacionFamiliar"]; 
+        let newEquipmentsFamiliar = equipments.slice(0,4)
+        alert(`Costo habitacion Familiar ${ habitacion["habitacionFamiliar"]}$ \n invitados: ${capturarPersonas} \n noches ${capturarNoches} \n total ${operacion}$
+        \n incluye: ${newEquipmentsFamiliar} `)
+    } 
+    else if (capturarHabitacion == "suit"){
+        operacion =  capturarPersonas * capturarNoches * habitacion["habitacionSuit"];
+        alert(`Costo habitacion Suit ${habitacion["habitacionSuit"]}$ \n invitados: ${capturarPersonas} \n noches ${capturarNoches} \n total ${operacion}$
+        \n incluye: ${equipments} `);
     }
-}
-
-//ejecutar programa
-function reserva() {
-habitaciones.forEach(habitacion => {
-//preguntar que tipo de habitacion
-let tipoDeHabitacion = prompt('1-habitacion basica (40$),  2-habitacion doble (50$),  3-habitacion familiar (60$), 4-habitacion suit (90$)');
-if( tipoDeHabitacion == 1 ){
-let newEquipmentsBasica = equipments.slice(0,2)
-resultado = resultadoPersonas * habitacion["habitacionBasica"] * resultadoNoche;  //CALCULO DE PRECIO
-//RESPUESTA total
-return  alert (`costo de habitacion basica: ${habitacion["habitacionBasica"]}$, 
-para ${resultadoPersonas} personas \n por ${resultadoNoche} noches. \n total = ${resultado}$
-\n INCLUYE: ${newEquipmentsBasica}`);
-
-} else if ( tipoDeHabitacion == 2) {
-let newEquipmentsDoble = equipments.slice(0,3)
-resultado = resultadoPersonas * habitacion["habitacionDoble"] * resultadoNoche;
-return alert(`Costo de habitacion Doble ${habitacion["habitacionDoble"]}$ \n para ${resultadoPersonas} personas \n por ${resultadoNoche} noches.  \n total =  ${resultado}$  \n INCLUYE: ${newEquipmentsDoble} `)
-
-} else if ( tipoDeHabitacion == 3) {
-    let newEquipmentsFamiliar = equipments.slice(0,4)
-    resultado = resultadoPersonas * habitacion["habitacionFamiliar"] * resultadoNoche;
-   return alert (`Costo habitacion Familiar ${ habitacion["habitacionFamiliar"]}$ \n para ${resultadoPersonas} personas \n por ${resultadoNoche} noches.  \n total  ${resultado}$  \n INCLUYE: ${newEquipmentsFamiliar} `);
-
-}else if ( tipoDeHabitacion == 4) {  
-resultado = resultadoPersonas * habitacion["habitacionSuit"] * resultadoNoche;
-return  alert (`Costo habitacion Suit ${habitacion["habitacionSuit"]}$ \n para ${resultadoPersonas} personas \n por ${resultadoNoche} noches.  \n total  ${resultado}$ \n INCLUYE: ${equipments}`);
-
-} else  {
-    return  alert("error, ingrese un numero valido");
-}
 });
 }
 
+calcular();
 }, false);
-
